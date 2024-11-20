8af60d669f595575fb356e65d468862f86557d53
sefdogann <sefer3850@gmail.com>
2024-11-20 10:10:05 +0300

    Add project files.

---
+++ CSharpEgitimKampi.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Update(T entity); 
+
+        void Delete(int id);
+
+        List<T> Getall();
+
+        T GetById(int id);
+    }
+}
